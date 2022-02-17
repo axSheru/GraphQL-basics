e3bdd777fbca9a5b237fee5a4fcc6b89d4aab693
Alex Paredes <alexparedesr95@gmail.com>
2022-02-17 16:05:57 -0600

    FEAT: Created basic types and resolvers; Created server and instruction to start it.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('Hello GraphQL!');
+import { GraphQLServer } from 'graphql-yoga';
+
+// Type definition (Schema).
+// The ! is to indicate it will allways be the return type value.
+const typeDefs = `
+    type Query {
+        hello: String!,
+        name: String!,
+        location: String!,
+        bio: String!,
+    }
+`;
+
+
+// Resolvers. The querys.
+const resolvers = {
+    Query: {
+        hello() {
+            return 'Hello! This is my first query!';
+        },
+        name() {
+            return 'Alex P';
+        },
+        location() {
+            return 'Puebla, México.';
+        },
+        bio() {
+            return 'I love SW!!!';
+        },
+    }
+};
+
+
+// Creates the server.
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+
+// Starts the server.
+server.start(() => {
+    console.log('Server up!')
+});
+

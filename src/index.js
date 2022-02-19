08696248627c60bc21a5225d1e6947bed03977af
Alex Paredes <alexparedesr95@gmail.com>
2022-02-18 20:00:13 -0600

    FEAT: Added examples of querys expecting parameters.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { GraphQLServer } from 'graphql-yoga';
 // The ! is to indicate it will allways be the return type value.
 const typeDefs = `
     type Query {
+        greeting(name: String, position: String): String!
+        add( a: Float!, b: Float! ): Float!
         me: User!
         post: Post!
     },
@@ -29,6 +31,12 @@ const typeDefs = `
 // Resolvers. The querys.
 const resolvers = {
     Query: {
+        greeting( parent, args, ctx, info ) {
+            return ( args.name && args.position ) ? `Hello ${ args.name }! You're my favorite ${ args.position }` : 'Hello!';
+        },
+        add( parent, args, ctx, info ) {
+            return ( args.a && args.b ) ? args.a + args.b : 'Enter numbers';
+        },
         me() {
             return {
                 id: 'm9803j',

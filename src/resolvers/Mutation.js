e403b1dd4223f92d5bf04ec287cf549e5f4d7d2a
Alex Paredes <alexparedesr95@gmail.com>
2022-03-01 16:45:27 -0600

    REFACTOR: Updated methods order to have a best order in the file.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -17,41 +17,6 @@ const Mutation = {
         
         return user;
     },
-    createPost( parent, args, { db }, info ) {
-        const userExists = db.users.some( ( user ) => user.id === args.data.author );
-
-        if( ! userExists ) {
-            throw new Error( 'User not found' );
-        }
-
-        const post = {
-            id: uuidv4(),
-            ...args.data
-        };
-
-        db.posts.push( post );
-
-        return post;
-
-    },
-    createComment( parent, args, { db }, info ) {
-        const userExists = db.users.some( ( user ) => user.id === args.data.author );
-        const postPublished = db.posts.some( ( post ) => ( post.id === args.data.post && post.published) );
-
-        if( ! userExists || ! postPublished ) {
-            throw new Error( 'Unable to find what you are looking for.' );
-        }
-
-        const comment = {
-            id: uuidv4(),
-            ...args.data
-        };
-
-        db.comments.push( comment );
-
-        return comment;
-
-    },
     deleteUser( parent, args, { db }, info ) {
         const userIndex = db.users.findIndex( ( user ) => user.id === args.id );
 
@@ -73,6 +38,23 @@ const Mutation = {
 
         return deletedUser[0];
     },
+    createPost( parent, args, { db }, info ) {
+        const userExists = db.users.some( ( user ) => user.id === args.data.author );
+
+        if( ! userExists ) {
+            throw new Error( 'User not found' );
+        }
+
+        const post = {
+            id: uuidv4(),
+            ...args.data
+        };
+
+        db.posts.push( post );
+
+        return post;
+
+    },
     deletePost( parent, args, { db }, info ) {
         const postIndex = db.posts.findIndex( ( post ) => post.id === args.id );
 
@@ -85,6 +67,24 @@ const Mutation = {
         db.comments = db.comments.filter( ( comment ) => comment.post !== args.id );
 
         return deletedPost[0];
+    },
+    createComment( parent, args, { db }, info ) {
+        const userExists = db.users.some( ( user ) => user.id === args.data.author );
+        const postPublished = db.posts.some( ( post ) => ( post.id === args.data.post && post.published) );
+
+        if( ! userExists || ! postPublished ) {
+            throw new Error( 'Unable to find what you are looking for.' );
+        }
+
+        const comment = {
+            id: uuidv4(),
+            ...args.data
+        };
+
+        db.comments.push( comment );
+
+        return comment;
+
     },
     deleteComment( parent, args, ctx, info ) {
         const commentIndex = db.comments.findIndex( ( comment ) => comment.id === args.id );
